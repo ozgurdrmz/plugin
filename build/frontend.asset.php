1e34e0216f71ed8bc43154b6daa5f99cba3a4182
ozgurdrmz <ozgurdrmz@gmail.com>
2024-04-19 11:56:00 +0300

    ozgur commit message

---
+++ build/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '7d3b93018d0a5ffb3051');
---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-element'), 'version' => '16d1a182c124c1dd479b');
