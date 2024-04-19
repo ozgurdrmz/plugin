d3fc23522abd4db65ddd662bacc11241ed1ec794
ozgurdrmz <142833817+ozgurdrmz@users.noreply.github.com>
2024-04-19 12:31:51 +0000

    silme

--- build/frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '7d3b93018d0a5ffb3051');
--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '16d1a182c124c1dd479b');
